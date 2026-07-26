c0c96918f70ce536c04f82ca72432e7db2cebbb6
Wenjie Mao <maow015@gmail.com>
2026-07-26 05:21:47 -0700

    Initial Commit

---
+++ Aster/AsterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsterApp.swift
+//  Aster
+//
+//  Created by serein on 2026/7/26.
+//
+
+import SwiftUI
+
+@main
+struct AsterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
